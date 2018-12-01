6985b21f9e2359a0f3dccccc30570a33b9150607
Unknown <unknown@example.com>
2018-12-01 02:24:09 -0700

    protobuf 3.6.1 gradle example

---
+++ src/main/java/app/App.java
@@ -0,0 +1,28 @@
+package main.java.app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        AddressBookProtos.Person.Builder person = AddressBookProtos.Person.newBuilder();
+        person.setId(1);
+        person.setName("James Callender");
+        person.setEmail("jcallender@bitbuckets.org");
+
+        AddressBookProtos.Person temp = person.build();
+
+
+        AddressBookProtos.Person john =
+        AddressBookProtos.Person.newBuilder()
+            .setId(1234)
+            .setName("John Doe")
+            .setEmail("jdoe@example.com")
+            .addPhones(
+                AddressBookProtos.Person.PhoneNumber.newBuilder()
+                .setNumber("555-4321")
+                .setType(AddressBookProtos.Person.PhoneType.HOME))
+            .build();
+
+        System.out.println("Hello World");
+
+    }
+}
