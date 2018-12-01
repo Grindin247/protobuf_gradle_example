a35f0457af0c063cba01684bc5ba16b9b51d300e
Unknown <mrjamescallender@gmail.com>
2018-12-01 13:00:53 -0700

    Runnable python client

--- src/main/python/udpClient.py
+++ src/main/python/udpClient.py
@@ -1,18 +1,33 @@
 import socket
 import time
+import sys
+
+import addressbook_pb2
+
+def flush_out(string, *arg):
+    print(string.format(arg))
+    sys.stdout.flush()
+
+person = addressbook_pb2.Person()
+person.id = 1234
+person.name = "From Python"
+person.email = "python@example.com"
+phone = person.phones.add()
+phone.number = "555-4321"
+phone.type = addressbook_pb2.Person.HOME
 
 UDP_IP = "192.168.0.3"
 UDP_PORT = 8000
 MESSAGE = "Hello, World!".encode('utf-8')
 
-print ("UDP target IP:", UDP_IP)
-print ("UDP target port:", UDP_PORT)
-print ("message:", MESSAGE)
+flush_out ("UDP target IP:", UDP_IP)
+flush_out ("UDP target port:", UDP_PORT)
+flush_out ("message:", MESSAGE)
 
 
 sock = socket.socket(socket.AF_INET, # Internet
              socket.SOCK_DGRAM) # UDP
 while True:
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    sock.sendto(person.SerializeToString(), (UDP_IP, UDP_PORT))
     time.sleep(0.1)
-    print('SENT!')
+    flush_out('SENT!')
