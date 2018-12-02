9dd0d0b437ab5e5bb3d39dde2843da49565c3701
Unknown <mrjamescallender@gmail.com>
2018-12-01 18:06:21 -0700

    Workaround message alignment issue. Not worth solving now. Having issues with Java clearing the receive buffer after a read

--- src/main/java/app/SampleMsgs.java
+++ src/main/java/app/SampleMsgs.java
@@ -1,5 +1,8 @@
 package app;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SampleMsgs {
     public static AddressBookProtos.Person pythonServerPerson =
     AddressBookProtos.Person.newBuilder()
@@ -26,8 +29,8 @@ public class SampleMsgs {
     public static AddressBookProtos.Person javaServerPerson =
     AddressBookProtos.Person.newBuilder()
         .setId(3)
-        .setName("From Java Server")
-        .setEmail("javaserver@example.com")
+        .setName("From Java Server__")
+        .setEmail("javaserver__@example.com")
         .addPhones(
             AddressBookProtos.Person.PhoneNumber.newBuilder()
             .setNumber("555-4321")
@@ -37,12 +40,23 @@ public class SampleMsgs {
     public static AddressBookProtos.Person javaClientPerson =
     AddressBookProtos.Person.newBuilder()
         .setId(4)
-        .setName("From Java Client")
-        .setEmail("javaclient@example.com")
+        .setName("From Java Client__")
+        .setEmail("javaclient__@example.com")
         .addPhones(
             AddressBookProtos.Person.PhoneNumber.newBuilder()
             .setNumber("555-4321")
             .setType(AddressBookProtos.Person.PhoneType.HOME))
         .build();
+
+     public static final Map<Integer, Integer> IdToSize = new HashMap<Integer, Integer>() {
+
+        {
+            put(1,62);
+            put(2,62);
+            put(3,62);
+            put(4,62);
+        }
+    };
+
 
 }
